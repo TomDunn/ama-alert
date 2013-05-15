a79b83526d39307d3a582716043b8e798d867f8e
tom dunn <tidunn13@gmail.com>
2013-05-14 22:58:06 -0400

    Added a helper function for printing the keywords for rules. Long title lines are truncated

--- server.js
+++ server.js
@@ -16,4 +16,12 @@ app.use(app.router);
 /* set up the routes */
 require('./routes')(app, Models, Text.tokenize);
 
+/* add some helper functions for jade */
+app.locals.showListOrEmpty = function(list, separator, emptyMsg) {
+    if (list.length == 1 && list[0] == '')
+        return emptyMsg;
+
+    return list.join(separator);
+}
+
 app.listen(1080);
